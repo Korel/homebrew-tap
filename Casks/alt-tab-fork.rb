8824b5cac271da9ae70a41eff0d1a0fd1d296e07
Korel <reyan-korel.erben@prokube.ai>
2026-09-09 11:24:23 +0200

    alt-tab-fork: migrate postflight to postflight_steps

--- Casks/alt-tab-fork.rb
+++ Casks/alt-tab-fork.rb
@@ -13,9 +13,11 @@ cask "alt-tab-fork" do
 
   # This DMG is not signed or notarized. Strip the quarantine flag the OS adds
   # on download so Gatekeeper does not block the app as "damaged" on launch.
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-dr", "com.apple.quarantine", "#{appdir}/AltTab.app"]
+  postflight_steps do
+    run "/usr/bin/xattr",
+        args:           ["-dr", "com.apple.quarantine", "{{appdir}}/AltTab.app"],
+        writable_paths: ["AltTab.app"],
+        writable_base:  :appdir
   end
 
   zap trash: [
